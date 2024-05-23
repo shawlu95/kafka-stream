58f9bdffa38a3b47946275a9aec523e2f2923414
Shaw Lu <shawlu95@gmail.com>
2024-05-23 04:13:16 -0700

    completing word count app

--- stream-starter-word-count/src/main/java/com/shawlu/kafka/stream/starter/word/count/StreamsStarterApp.java
+++ stream-starter-word-count/src/main/java/com/shawlu/kafka/stream/starter/word/count/StreamsStarterApp.java
@@ -2,8 +2,13 @@ package com.shawlu.kafka.stream.starter.word.count;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class StreamsStarterApp {
@@ -14,5 +19,20 @@ public class StreamsStarterApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+        KStream<String, String> input = builder.stream("word-count-input"); // name of topic
+        KTable<String, Long> wc = input
+            .mapValues(x -> x.toLowerCase())
+            .flatMapValues(x -> Arrays.asList(x.split("\\W+"))) // split by one or more non-word
+            .selectKey((k, v) -> v) // set key, before group by
+            .groupByKey()
+            .count("counts"); // queryable store name
+
+        // key is string, value is long
+        wc.to(Serdes.String(), Serdes.Long(), "word-count-output");
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
     }
 }
